8a92cdd4c06b4d2b16c7235ef7502d7f905fd0ca
minhtamtkst21 <minhtamtruongkhanh.st@gmail.com>
2019-10-01 15:27:52 +0700

    sfss

---
+++ Exercise 04/Exercise 04/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_04
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            InputInt(arr);
+            Console.WriteLine(IsContain5(arr));
+        }
+
+        public static void InputInt(int[]arr)
+        {
+            for(int i=0; i < arr.Length; i++)
+            {
+                Console.Write("A[{0}] = ", i);
+                string str = Console.ReadLine();
+                arr[i] = int.Parse(str);
+            }
+        }
+
+        public static bool IsContain5(int[]arr)
+        {
+            for (int i = 0; i < arr.Length; i++ )
+            {
+                if (arr[i] == 5)
+                    return true;
+            }
+                return false;
+        }
+
+        public static bool IsContain5_or_6(int[] arr)
+        {
+            return false;
+        }
+
+        public static bool IsContain5_and_6(int[] arr)
+        {
+            return false;
+        }
+    }
+}
---
+++ Exercise 04/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise_04;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] arr1 = new int[] { 1, 2, 3, 4 };
+            bool result0 = Program.IsContain5(arr1);
+            Assert.AreEqual(false, result0);
+
+            int[] arr2 = new int[] { 1, 2, 3, 4, 5};
+            bool result3 = Program.IsContain5(arr2);
+            Assert.AreEqual(true, result3);
+        }
+    }
+}
