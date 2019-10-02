2de90a85b032cdfb0df864c8ce16f13706a910d4
Tran Tien Dat <bryce.gazelle2000@gmail.com>
2019-10-02 21:35:52 +0700

    Chua xongggg

--- Exercise 04/Exercise 04/Program.cs
+++ Exercise 04/Exercise 04/Program.cs
@@ -10,9 +10,13 @@ namespace Exercise_04
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[10];
+            int[] arr = new int[5];
             InputInt(arr);
             Console.WriteLine(IsContain5(arr));
+            Console.WriteLine(IsContain5_or_6(arr));
+            Console.WriteLine(IsContain5_and_6(arr));
+            Console.WriteLine(Count5(arr));
+            Console.WriteLine(Count5or6(arr));
         }
 
         public static void InputInt(int[]arr)
@@ -27,8 +31,9 @@ namespace Exercise_04
 
         public static bool IsContain5(int[]arr)
         {
+            Console.Write("IsContaint5 is: ");
             for (int i = 0; i < arr.Length; i++ )
-            {
+            {        
                 if (arr[i] == 5)
                     return true;
             }
@@ -37,12 +42,70 @@ namespace Exercise_04
 
         public static bool IsContain5_or_6(int[] arr)
         {
-            return false;
+            Console.Write("IsContains 5 or 6 is: ");
+            for (int i = 0; i < arr.Length; i++)
+            {   
+                if (arr[i] == 5 || arr[i] == 6)
+                    return true;
+            }
+                return false;
         }
 
         public static bool IsContain5_and_6(int[] arr)
         {
-            return false;
+            //Console.Write("Is contains 5 and 6 is: ");
+            //bool five = false, six = false;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] == 5) five = true;
+            //    else if (arr[i]==6) six = true;
+            //}
+            //if (five && six == true) return true;
+            //else return false;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 5)
+                {
+                    for (int j = 0; j < arr.Length; j++)
+                    {
+                        if (arr[j] == 6)
+                            return true;
+                    }
+                }
+            }return false;
+        }
+
+        public static int Count5(int[] arr)
+        {
+            int dem = 0;
+            for(int i =0; i< arr.Length; i++)
+            {
+                if (arr[i] == 5)
+                    dem++;
+            }
+            Console.Write("So phan tu 5 xuat hien {0} lan", dem);
+            return dem;
+        }
+
+        public static int Count5or6(int[]arr)
+        {
+            int dem = 0;
+            for(int i=0; i<arr.Length;i++)
+            {
+                if (arr[i] == 5)
+                {
+                    dem++;
+                    Console.Write("Phan tu 5 xuat hien {0} lan ", dem);
+                }
+                else if (arr[i] == 6)
+                {
+                    dem++;
+                    Console.Write("Phan tu 6 xuat hien {1} lan ", dem);
+                }
+            }
+            
+            return dem;
         }
     }
 }
--- Exercise 04/UnitTestProject1/UnitTest1.cs
+++ Exercise 04/UnitTestProject1/UnitTest1.cs
@@ -13,8 +13,49 @@ namespace UnitTestProject1
             bool result0 = Program.IsContain5(arr1);
             Assert.AreEqual(false, result0);
 
-            int[] arr2 = new int[] { 1, 2, 3, 4, 5};
+            int[] arr2 = new int[] { 1, 2, 3, 4, 5 };
             bool result3 = Program.IsContain5(arr2);
+            Assert.AreEqual(true, result3);
+        }
+
+        [TestMethod]
+        public void Test5or6()
+        {
+            int[] arr1 = new int[] { 1, 2, 3, 4 };
+            bool result = Program.IsContain5_or_6(arr1);
+            Assert.AreEqual(false, result);
+
+            int[] arr2 = new int[] { 1, 2, 3, 4, 5 };
+            bool result1 = Program.IsContain5_or_6(arr2);
+            Assert.AreEqual(true, result1);
+
+            int[] arr3 = new int[] { 1, 2, 3, 4, 6 };
+            bool result2 = Program.IsContain5_or_6(arr3);
+            Assert.AreEqual(true, result2);
+
+            int[] arr4 = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            bool result3 = Program.IsContain5_or_6(arr4);
+            Assert.AreEqual(true, result3);
+        }
+
+
+        [TestMethod]
+        public void Test5and6()
+        {
+            int[] arr1 = new int[] { 1, 2, 3, 4 };
+            bool result = Program.IsContain5_and_6(arr1);
+            Assert.AreEqual(false, result);
+
+            int[] arr2 = new int[] { 1, 2, 3, 4, 5 };
+            bool result1 = Program.IsContain5_and_6(arr2);
+            Assert.AreEqual(false, result1);
+
+            int[] arr3 = new int[] { 1, 2, 3, 4, 6 };
+            bool result2 = Program.IsContain5_and_6(arr3);
+            Assert.AreEqual(false, result2);
+
+            int[] arr4 = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            bool result3 = Program.IsContain5_and_6(arr4);
             Assert.AreEqual(true, result3);
         }
     }
